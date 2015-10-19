336bb37d8b997a3773c9d0e3a586f23bd23867c9
Isaac Stead <isaac.stead@protonmail.com>
2015-10-19 16:46:52 +1300

    Added a setup.py - now it's a real Python package! :D

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup (
+    name='ash',
+    version='dev',
+    description='ash is a Markov chain based random poetry generator.',
+    author='Isaac Stead',
+    author_email='isaac.stead@protonmail.com',
+    url='https://github.com/isaac-rks/ash',
+    packages=['ash'],
+    install_requires=[
+        'networkx',
+        'nltk',
+        'ply',
+        'regex'
+    ]
+)
