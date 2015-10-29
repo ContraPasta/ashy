7b79f03e1d4331ef18b3b2f7287e10be95fac562
Isaac Stead <isaac.stead@protonmail.com>
2015-10-29 22:17:45 +1300

    Got poetic device menu, word selection for device working :)

--- app/static/poembuilder.js
+++ app/static/poembuilder.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
+
     var uiState = 'normal';
+    var poemData = {};
+    var selectedWordID;
+    var selectedDevice;
+
     // Load HTML for word control from server
     var wordUiElement;
     $.get('/static/word_ui_element.html', function (data) {
@@ -17,20 +22,22 @@ $(document).ready(function () {
 	nwords++;
     });
 
+    $('#ui-clear-words').click(function (event) {
+	$('#ui-spec').empty();
+    });
+
     $('#ui-change-state').click(function (event) {
 	if (uiState === 'word_select') {
 	    uiState = 'normal';
 	} else {
 	    uiState = 'word_select';
-	    // Switch off event handlers on the dropdowns to prevent the
-	    // bootstrap event interfering with custom event
 	}
     });
 
     // NB: When binding to elements that are created dynamically you need to
     // use jQuery.fn.on on an ancestor element that exists at the time the
     // event is bound.
-    $('#ui-spec').on('click', '.dropdown .dropdown-toggle', function () {
+    $('#ui-spec').on('click', '.dropdown .dropdown-toggle', function (event) {
 	if (uiState === 'normal') {
 	    $(this).dropdown('toggle');
 	    // Switch off event handlers on the dropdowns to prevent the
@@ -38,10 +45,37 @@ $(document).ready(function () {
 	    $('.dropdown-toggle').unbind();
 	}
 	else {
-	    testClickCounter ++;
-	    console.log('testclick incremented');
+	    var id = $(this).attr('id');
+	    console.log(id, selectedWordID);
+	    if ( id === selectedWordID ) {
+		uiState = 'normal';
+		$(this).dropdown('toggle');
+		$(this).text('Word');
+	    }
+	    else {
+		if ( selectedDevice in poemData ) {
+		    if ( poemData[selectedDevice].indexOf(id) < 0 ) {
+			poemData[selectedDevice].push(id);
+		    }
+		}
+		else {
+		    poemData[selectedDevice] = [id];
+		}
+		console.log(poemData);
+	    }
 	}
     });
 
+    // Bind event handlers for word control menu options
+    $('#ui-spec').on('click', '.dropdown  #word-rhyme', function (event) {
+	console.log('rhyme clicked');
+	uiState = 'word_select';
+	$('.dropdown-toggle').unbind();
+	var parentButton = $(this).closest('.dropdown-menu').prev();
+	var wordID = parentButton.attr('id');
+	selectedWordID = wordID;
+	selectedDevice = 'rhyme';
+	parentButton.text('Done');
+    });
 
 });
